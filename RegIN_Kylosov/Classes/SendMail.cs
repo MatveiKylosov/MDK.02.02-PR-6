90b1ff43e366a6855db6154c92450604c6b0032f
matvei <matveikylosov@gmail.com>
2024-11-25 15:32:48 +0500

    Разработаанный класс отправки сообщения на почту. Фамилия

---
+++ RegIN_Kylosov/Classes/SendMail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows;
+
+namespace RegIN_Kylosov.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
